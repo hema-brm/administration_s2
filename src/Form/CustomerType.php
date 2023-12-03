8f8dd40de9fc817d57f803b1d0f1555f5ec5d702
johan-mickael-myges <jrakotoniaina2@myges.fr>
2023-12-03 20:58:41 +0100

    Add button and input macros

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -4,20 +4,85 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CustomerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('address')
-            ->add('phone')
-        ;
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'attr' => [
+                    'placeholder' => 'Entrez l\'adresse email',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une adresse email',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez entrer une adresse email valide',
+                    ]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom',
+                    ]),
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez le prénom',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un prénom',
+                    ]),
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'help' => 'ex: 1 rue de la Paix',
+                'attr' => [
+                    'placeholder' => 'Entrez une adresse',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une adresse',
+                    ]),
+                ],
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Numéro de téléphone',
+                'help' => 'ex: 0123456789',
+                'attr' => [
+                    'placeholder' => 'Entrez un numéro de téléphone',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un numéro de téléphone',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Le numéro de téléphone doit contenir au moins {{ limit }} caractères',
+                        'max' => 15,
+                        'maxMessage' => 'Le numéro de téléphone doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
