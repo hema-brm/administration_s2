813c4f0dbbc648545cdb8eb1381c1204ae9d2d76
johan-mickael-myges <jrakotoniaina2@myges.fr>
2023-12-06 23:51:57 +0100

    Add custom type tsvector for fullsearch

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use App\Doctrine\TsvectorType;
+use Doctrine\DBAL\Types\Type;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        if (!Type::hasType('tsvector')) {
+            Type::addType('tsvector', TsvectorType::class);
+        }
+    }
 }
