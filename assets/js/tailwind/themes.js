e846d45c749f63713499a141117e4d9e5a5848f5
johan-mickael-myges <jrakotoniaina2@myges.fr>
2023-12-18 23:06:44 +0100

    Refactor tailwind colors

---
+++ assets/js/tailwind/themes.js
@@ -0,0 +1,17 @@
+const availableThemes = [
+    'theme-blue',
+    'theme-yellow',
+    'theme-green',
+    'theme-red',
+    'theme-teal',
+    'theme-orange',
+    'theme-gold',
+    'theme-silver',
+    'theme-black',
+    'theme-pink',
+    'theme-purple',
+    'theme-indigo',
+    'theme-coffee',
+];
+
+export default availableThemes;
