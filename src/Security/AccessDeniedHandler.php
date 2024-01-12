67e789dee172f8033cb34c996dca441d7db844cd
johan-mickael-myges <jrakotoniaina2@myges.fr>
2024-01-13 00:21:45 +0100

    Fix access denied handler

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -11,8 +12,19 @@ class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
     public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
     {
-        // Add a flash message
-        return new Response(null, 404);
+
+        $referer = $request->headers->get('referer');
+
+        if (empty($referer)) {
+            $referer = '/';
+        }
+
+        $response = new RedirectResponse($referer);
+
+        $session = $request->getSession();
+        $session->getFlashBag()->add('error', 'Vous n\'avez pas les droits pour accéder à cette page.');
+
+        return $response;
     }
 }
 
