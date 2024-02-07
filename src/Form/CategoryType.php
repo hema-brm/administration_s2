1a2fb51dba9c39008a5888cd3564606b9447aa99
hema-brm <hema932001@gmail.com>
2024-02-07 22:08:57 +0100

    traduction categoryform

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -14,6 +14,7 @@ class CategoryType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, [
+                'label' => 'Nom de la catégorie',
                 'required'    => true
             ])
         ;
