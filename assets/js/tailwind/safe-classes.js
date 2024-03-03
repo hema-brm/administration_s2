58d990452d933cad3950200c91cdaac93f4beb20
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-03-03 02:15:03 +0100

    Adding more safe classlist

--- assets/js/tailwind/safe-classes.js
+++ assets/js/tailwind/safe-classes.js
@@ -1,10 +1,17 @@
 const availableTailwindSafeClasses = [
     {
-        pattern: /^(-)?(m([lrtb])?|max-w|min-w|max-h|min-h)-(\d+|\d+(\.\d+)?)(.*)$/
+        pattern: /^(-)?(m([lrtb])?|max-w|min-w|max-h|min-h)-(\d+|\d+(\.\d+)?)(.*)$/,
+        variants: ['xs', 'sm', 'md', 'lg', 'xl', '2xl']
     },
     {
-        pattern: /^(bg|text|border|ring|divide|placeholder)-((primary|danger|warning|success|info)|[a-z]+-\d{1,3})-?$/
+        pattern: /^(bg|text|border|rounded|ring|shadow|divide|placeholder)-((primary|danger|warning|success|info)|[a-z]+-\d{1,3})-?$/,
+        variants: ['dark', 'xs', 'sm', 'md', 'lg', 'xl', '2xl']
     },
+    {
+        // pattern for tailwind rounded size or text size or shadow size
+        pattern: /^(rounded|text|shadow)-([a-z]+)-?$/,
+        variants: ['xs', 'sm', 'md', 'lg', 'xl', '2xl']
+    }
 ];
 
 export default availableTailwindSafeClasses;
